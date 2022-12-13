aa744bae12987c05857ca32842cbe7bbb56ba8fc
PengY <54921185+pengyofficial@users.noreply.github.com>
2022-12-13 11:38:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const app = require('express')()
-const PORT = 8080
+const express = require('express');
+const app = express()
+const PORT = 8080;
 
-app.get('/bob', (req,res) => {
-    res.status(200).send({
-        "name": "Bob Ducan",
-        "age": 23
-    })
-})
+app.use( express.json() )
 
 app.listen(
     PORT,
     () => console.log(`Im alive on https://localhost${PORT}`)
 )
+
+app.get('/tshirt', (req,res) => {
+    res.status(200).send({
+        "tshirt": "Bob Ducan",
+        "size": 'Large'
+    })
+})
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params
+    const { logo } = req.body
+
+    if (!id) {
+        res.status(418).send({ message: 'Need a ID'})
+    }
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!'})
+    }
+
+    res.send({
+        tshirt: `With your ${logo} of a ID of ${id}`
+    })
+})
+
