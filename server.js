475a90a80605ff702e5287152f52a831bf97b4c8
EchoGronkowski <robbysmainemail@gmail.com>
2023-01-01 19:55:43 -0500

    New Structure

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
 // Server Constants
+const fs = require('fs');
 const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 5000
+const apiName = "ImDragons"
 
 // Data Access Constants
 const band = require('./api/data/band.json')
-const songlist = require('./api/data/songlist.json')
-const dataEntries = ["band", "album", "songlist"]
+const songs = require('./api/data/songs.json')
+const dataEntries = ["band", "album", "songs"]
+const home = fs.readFileSync('./api/home.html')
 
-// Use Express 
+// JSON Middleware
 app.use(express.json())
 
+// Root Directory of api.unnecessarylibraries.com
 app.get('/', (req, res) => {
     res.status(200).send(
         JSON.stringify({
-            message: 'Route Not Found: Please use the /imaginejs endpoint',
+            message: 'Route Not Found: Please use the /ImDragons endpoint',
         })
     )
 })
 
-// Get Request
-app.get('/imaginejs', (req, res) => {
-    res.status(200).send("Welcome to the Imaginedragons.js API.")
+// Get Request for api.unnecessarylibraries.com
+app.get(`/${apiName}`, (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(home);
+
 })
 
-app.get('/imaginejs/data', (req, res) => {
-    res.status(200).send({ band, songlist })
+// Get Request for https://api.unnecessarylibraries.com/imaginejs/data
+app.get(`/${apiName}/data`, (req, res) => {
+    res.status(200).send({ band, songs })
 })
 
-app.get('/imaginejs/data/:jsonEntry', (req, res) => {
+app.get(`/${apiName}/data/:jsonEntry`, (req, res) => {
 
-    const entry = req.params.jsonEntry
-    const response = require(`./api/data/${entry}.json`)
 
-    if (!entry) {
-        res.status(418).send({ message: 'Needs a Data ID' })
-    }
+    const param = req.params.jsonEntry
+    const entry = param.toLowerCase()
 
-    if (dataEntries.indexOf(entry) > -1) {
+    if (dataEntries.includes(entry) == true) {
+        const response = require(`./api/data/${entry}.json`)
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(response)
     } else {
-        res.status(404).send({ message: `No Such Data Entry could be found.` })
+        res.status(404).send({ message: `No Such Data route could be found. Please Look back at available routes` })
     }
+
+
 })
 
 app.listen(
