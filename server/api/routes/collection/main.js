6d579933dc1731b2591f6389151d217c3273b107
RobbiDev <robbysmainemail@gmail.com>
2023-01-23 09:02:28 -0500

    1.0.1

---
+++ server/api/routes/collection/main.js
@@ -0,0 +1,78 @@
+// Express / Router
+const express = require('express')
+const router = express.Router()
+
+// Import Configuration
+const config = require('../../config.json')
+
+// Imports
+const apiEntries = config.entries
+
+// Middleware
+router.use(express.json())
+
+// Request Handler
+router.get('/', (req, res) => {
+
+        const file = {
+            "Collection-Types": [
+                "lists", "infomation"
+            ],
+            "Examples": {
+                "Album list": "/imdragons/v1/collection/list/album",
+                "Song list": "/imdragons/v1/collection/list/song"
+            }
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(file)
+
+})
+
+router.get(`/:contentType`, (req, res) => {
+
+    const cat = req.params.category.toLowerCase()
+    const type = req.params.contentType.toLowerCase()
+
+    if (apiEntries.category.includes(cat) == true) {
+
+        const file = {
+            "Content In List Collection": [
+                "album", "songs", "awards"
+            ],
+            "Examples": {
+                "Album list": "/imdragons/v1/collection/list/album",
+                "Song list": "/imdragons/v1/collection/list/song",
+                "Awards list": "/imdragons/v1/collection/list/awards"
+            }
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(file)
+    } else {
+        res.status(404).send({ message: `No Such Data route could be found. Please Look back at available routes` })
+    }
+
+})
+
+// GET Contents within a Collection Type
+router.get(`/:contentType/:content`, (req, res) => {
+
+    const type = req.params.contentType.toLowerCase()
+    const entry = req.params.content.toLowerCase()
+
+    if (apiEntries.type.includes(type) == true && apiEntries.contents.includes(entry) == true) {
+
+        const file = require(`../../../db/collection/${type}/${entry}.json`)
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(file)
+    } else {
+        res.status(404).send({ message: `No Such Data route could be found. Please Look back at available routes` })
+    }
+
+})
+
+// Export
+module.exports = router
+
