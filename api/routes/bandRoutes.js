48341ba69d425dc64efae5f976b0a173a21f16f1
Robby J <54921185+RobbiDev@users.noreply.github.com>
2024-06-15 21:00:15 -0400

    BAND ROUTE

--- api/routes/bandRoutes.js
+++ api/routes/bandRoutes.js
@@ -1,26 +1,40 @@
 // Imports
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 import { Success, Error } from '../util/ResponseHandler.js'
-import path, { dirname } from 'path';
-import express from 'express';
-import fs from 'fs';
+import path, { dirname } from 'path'
+import express from 'express'
+import fs from 'fs'
 
-const router = express.Router();
+const router = express.Router()
 
 // File Handling
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const FilePath = path.resolve(__dirname, '../data/band.json');
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const FilePath = path.resolve(__dirname, '../data/band.json')
 
 // Middleware to read JSON file
 function readFile() {
-  return JSON.parse(fs.readFileSync(FilePath, 'utf8'));
+  return JSON.parse(fs.readFileSync(FilePath, 'utf8'))
 }
 
 // GET /albums - Get all albums
 router.get('/', (req, res) => {
   try {
-    const band = readFile();
+    const band = readFile()
     Success(res, 200, band)
+  } catch (err) {
+    Error(res, 404, "fu")
+  }
+});
+
+// GET /albums - Get all albums
+router.get('/:type', (req, res) => {
+
+  const type = req.params.type
+  
+  try {
+    const band = readFile()
+    const data = band[`${type}`]
+    Success(res, 200, data)
   } catch (err) {
     Error(res, 404, "fu")
   }
