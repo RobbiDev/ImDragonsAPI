960fbfcc168c42ac373ca3554e1a848db2f5ef3a
Robby J <54921185+RobbiDev@users.noreply.github.com>
2024-07-19 00:21:09 -0400

    Added All of LOOM

--- test/song-lyrics.test.js
+++ test/song-lyrics.test.js
@@ -1,12 +1,10 @@
 // Testing for Accuracy of Lyrics and way its displayed
-import song from "../api/data/songs/EVOLVE_9_Thunder.json" assert { type: "json" }
+import song from "../api/data/songs/LOOM_2_Nice_to_Meet_You.json" assert { type: "json" }
 
 function output(lyrics){
-
     for (let i = 0; i < lyrics.length; i++) {
         const element = lyrics[i];
-        console.log(element)
-        console.log("\n")
+        console.log(`\n${element}`)
     }
 }
 
